cdc3b7ef54e2ff0c6146e3a160dbde4a23132683
xuliang <xuliang12187@gmail.com>
2021-03-29 10:29:45 +0800

    测试案例更新

--- test/test01.go
+++ test/test01.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+type Hander interface {
+	ServeHTTP(w http.ResponseWriter, r *http.Request)
+}
+
 func main() {
 	resp, err := http.Get("https://www.baidu.com")
 	if err != nil {
@@ -12,5 +16,22 @@ func main() {
 	}
 	defer resp.Body.Close()
 	//fmt.Println(resp.Body)
-	fmt.Printf("类型是%T，值是%v",resp,resp)
+	fmt.Printf("类型是%T，值是%v", resp, resp)
+	//http.DefaultServeMux.HandleFunc()
+
+	fmt.Println("#######")
+	Header := map[string][]string{
+		"Accept-Encoding": {"gzip, deflate"},
+		"Accept-Language": {"en-us"},
+		"Foo":             {"Bar", "two"},
+	}
+
+	fmt.Println(Header)
+
+	tester := map[string]int{"key01": 2}
+	fmt.Println(tester["key01"])
+
+	http.Handle
+
 }
+
